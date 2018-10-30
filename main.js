0df75d0c8f97acc15ec2c6a682fc0fbb735f1bef
carissa morrow <carissamorrow@hotmail.com>
2018-10-30 09:10:52 -0600

    additions

---
+++ main.js
@@ -0,0 +1,32 @@
+let bag = {
+  name: "Trail Mix",
+  shape: "square",
+  color: "blue"
+}
+let label1 = {
+  name: "Kirkland",
+  position: "top",
+  backgroundColor: "brown",
+  fontColor: "white",
+}
+let label2 = {
+  name: "net wt 57g",
+  position: "bottom",
+  fontColor: "black",
+  backgroundColor: "white",
+}
+let backLabel1 = {
+  name: "Ingredients",
+  color: "white",
+  fontColor: "black",
+}
+let backLabel2 = {
+  name: "Nutrition Facts",
+  fontColor: "black",
+  backgroundColor: "white",
+}
+let backLabel3 = {
+  name: "Allergen Information",
+  fontColor: "black",
+  backgroundColor: "white",
+}
