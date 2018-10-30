584581bb6610e8e28b0075b67f52f256459011dc
carissa morrow <carissamorrow@hotmail.com>
2018-10-30 09:17:41 -0600

    done

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 let bag = {
   name: "Trail Mix",
   shape: "square",
-  color: "blue"
+  color: "blue",
+  material: "plastic"
 }
 let label1 = {
   name: "Kirkland",
   position: "top",
   backgroundColor: "brown",
-  fontColor: "white",
+  fontColor: "white"
 }
 let label2 = {
   name: "net wt 57g",
   position: "bottom",
   fontColor: "black",
-  backgroundColor: "white",
+  backgroundColor: "white"
 }
 let backLabel1 = {
   name: "Ingredients",
   color: "white",
-  fontColor: "black",
+  fontColor: "black"
 }
 let backLabel2 = {
   name: "Nutrition Facts",
   fontColor: "black",
-  backgroundColor: "white",
+  backgroundColor: "white"
 }
 let backLabel3 = {
   name: "Allergen Information",
   fontColor: "black",
-  backgroundColor: "white",
+  backgroundColor: "white"
 }
+let contents = {
+  name: ["raisins, peanuts MM's, almonds, cashews"]
+}
+
+console.log(bag.name)
+console.log(backLabel3.name)
